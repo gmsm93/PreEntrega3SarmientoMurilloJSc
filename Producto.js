1e65e9749b4e436ecf4adf2407201c5cd0e34d59
gmsm93 <116608456+gmsm93@users.noreply.github.com>
2023-01-30 23:53:05 -0500

    Carga Inicial

---
+++ Producto.js
@@ -0,0 +1,11 @@
+class Producto {
+    names;
+    prices;
+    images;
+    
+    constructor(names, prices, images) {
+        this.names = names;
+        this.prices = prices;
+        this.images = images;
+    }
+}
